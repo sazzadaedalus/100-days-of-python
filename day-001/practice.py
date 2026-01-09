88088108bfca8268c77d9d3a24e9248334447e59
Sazzad <75588005+sazzadaedalus@users.noreply.github.com>
2026-01-09 20:38:46 +0000

    Create practice.py
    Day-01 Practice Exercises

---
+++ day-001/practice.py
@@ -0,0 +1,14 @@
+# First day of coding
+print("Hello world!")
+
+# String manipulation
+print("Hello world!\nHello World!")
+print("Hello" + " " + "Sazzad")
+
+# Inputs
+print("Hello " + input("What is your name? ") + "!")
+
+# Variables + length
+name = input("What is your name? ")
+name_length = len(name)
+print("Your name has " + str(name_length) + " characters")
